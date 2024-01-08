721c71fade6fd1556752c431ddcc980fe12f0226
Copianation <ruoyu.Xu@stud.uni-regensburg.de>
2024-01-08 15:55:16 +0100

    Add blatt 1, 6, 9

---
+++ blatt9/include/helper.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <stdlib.h>
+#include <stdio.h>
+
+typedef double* (*Map)(double*);
+
+void functionOutput(int dim, Map f, double* x, double* y);
+
+double** initEmptyMatrix(int m, int n);
+
+double** initMatrix(int m, int n, double* A);
+
+void addTo(double* v1, double* v2, int size);
+
+// Store -v in neg_v
+void negative(double* v, double* neg_v, int size);
+
+double dot(double* v1, double* v2, int size);
+
+void printVector(double* v, int n);
+
+void printMatrix(double** M, int m, int n);
+
+int* idPermutation(int size);
---
+++ blatt9/test/problem3.c
@@ -0,0 +1,11 @@
+#include <math.h>
+#include "Broyden.h"
+
+
+double* problem3Func(double* x)
+{
+}
+
+void main()
+{
+}
